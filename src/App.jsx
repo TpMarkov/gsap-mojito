c4acc7b49eea32775c28bf0a916f2a2decfa3a3f
Tsvetanmarkov <tsvetan.markov.a70@learn.telerikacademy.com>
2025-12-16 19:02:38 +0200

    cocktails list

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import {ScrollTrigger, SplitText} from "gsap/all";
 import gsap from "gsap";
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
+import Cocktails from "./components/Cocktails.jsx";
+
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -11,8 +13,8 @@ const App = () => {
       <main>
         <Navbar/>
         <Hero/>
+        <Cocktails/>
       </main>
-
   )
 }
 export default App
